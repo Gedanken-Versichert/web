a188014bcd9cde3abefe267dfeaf8f1dfd1f0ca1
MesterMan03 <MesterMan03+github@proton.me>
2023-11-07 00:32:54 +0100

    remove sitemap - it's generated from a different source

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
   site: "https://gedankenversichert.com"
 });
